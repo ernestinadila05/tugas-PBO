98645aaf0140ed74db5cb167cdda5fada693ea8d
ernestinadila05 <163815273+ernestinadila05@users.noreply.github.com>
2024-03-18 13:12:55 +0700

    Add files via upload

---
+++ latihan3pboA.java
@@ -0,0 +1,14 @@
+/*
+
+nama : nadila ernesti
+no nim : 2301091017
+soal no 3 for loop
+*/
+public class latihan3pboA {
+    public static void main(String[] args) {
+        for ( int i = 0; i <= 100;i++)
+        {
+            System.out.println("zaki zafran");
+        }
+    }
+}
---
+++ latihan3pboB.java
@@ -0,0 +1,15 @@
+/*
+
+nama : nadila ernesti
+no nim : 2301091017
+soal 3 do while
+*/
+public class latihan3pboB {
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.out.println("zaki zafran");
+            i++;
+        } while (i <= 100);
+    }
+}
